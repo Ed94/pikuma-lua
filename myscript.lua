a1921d97a8018f98eee850a575f8f1f28437b945
Ed_ <edwardgz@gmail.com>
2025-03-23 22:32:27 -0400

    vod 10

--- myscript.lua
+++
@@ -1,6 +0,0 @@
-print("Hello, world!")
-print(5 * 3)
-print("Thank you!")
-
-name = "Ed"
-print(name)
---
+++ vod_10.lua
@@ -0,0 +1,26 @@
+print("Examples of variables")
+
+score = 0
+lives = 3
+
+player1_name = "Lerroy Jenkins"
+player2_name = "Gustavo Pezzi"
+
+print(type(score))
+print(type(player1_name))
+
+-- Frontend:
+-- nil
+-- number
+-- string
+-- function
+
+-- Backend:
+-- CFunction
+-- userdata
+-- table
+
+salary = 25 / 2
+print("The value of slary is " .. salary)
+
+print("The value of name is " .. player1_name)
